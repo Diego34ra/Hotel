e1b17c5cde09446cc89352118836f2e9ea70c1ca
Diego Ribeiro <102061960+Diego34ra@users.noreply.github.com>
2024-07-22 10:26:44 -0300

    22-07-2024 v1

--- src/main/resources/db/migration/V3__insert_room.sql
+++ src/main/resources/db/migration/V3__insert_room.sql
@@ -1,5 +1,5 @@
 INSERT INTO hotel.tb_room (room_id, description, price, floor, available, capacity, type)
 VALUES
-(1, 'Quarto de luxo com vista para o mar', '150.75', 3, 1, 2,'DUPLO'),
-(2, 'Quarto standard com vista para a cidade', '100.50', 2, 1, 2,'DUPLO'),
-(3, 'Suíte presidencial com varanda privativa', '250.00', 5, 1, 4,'SUITE');
+(1, 'Quarto de luxo com vista para o mar', 150.75, 3, 1, 2,'DUPLO'),
+(2, 'Quarto standard com vista para a cidade', 100.50, 2, 1, 2,'DUPLO'),
+(3, 'Suíte presidencial com varanda privativa', 250.00, 5, 1, 4,'SUITE');
---
+++ src/main/resources/db/migration/V4__insert_hospitality.sql
@@ -0,0 +1,4 @@
+INSERT INTO hotel.tb_hospitality (hospitality_id, name, description, price)
+VALUES
+(1, 'Serviço de Quarto', 'Entrega de refeições, bebidas ou outros itens diretamente no quarto do hóspede', 29.90),
+(2, 'Aluguel de Equipamentos', 'Aluguel de bicicletas, equipamentos de esportes aquáticos ou outros itens recreativos', 34.90);

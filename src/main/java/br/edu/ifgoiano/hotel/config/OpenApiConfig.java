e198885db22c9e499b68aca6041a91caa4d06529
Diego Ribeiro <102061960+Diego34ra@users.noreply.github.com>
2024-07-24 07:33:45 -0300

    24-07-2024 v1

--- src/main/java/br/edu/ifgoiano/hotel/config/OpenApiConfig.java
+++ src/main/java/br/edu/ifgoiano/hotel/config/OpenApiConfig.java
@@ -1,12 +1,8 @@
 package br.edu.ifgoiano.hotel.config;
 
-import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
@@ -23,10 +19,6 @@ public class OpenApiConfig {
         devServer.setUrl("http://localhost:8080");
         devServer.setDescription("URL do servidor no ambiente de desenvolvimento");
 
-//        Server prodServer = new Server();
-//        prodServer.setUrl("https://projeto-ecommerce-production.up.railway.app");
-//        prodServer.setDescription("URL do servidor no ambiente de produção");
-
         Contact contact = new Contact();
         contact.setEmail("diegoribeiro13ra@hotmail.com");
         contact.setName("Diego Ribeiro Araújo");
@@ -35,14 +27,14 @@ public class OpenApiConfig {
 //        License mitLicense = new License().name("MIT License").url("https://choosealicense.com/licenses/mit/");
 
         Info info = new Info()
-                .title("Api Projeto Ecommerce")
+                .title("Api Projeto Hotel")
                 .version("1.0")
                 .contact(contact)
-                .description("Esta API apresenta os endpoints de um projeto de ecommerce.").termsOfService("");
+                .description("Esta API apresenta os endpoints de um projeto de reservas de quartos de um Hotel.").termsOfService("");
 //                .license(mitLicense);
 
         return new OpenAPI()
                 .info(info)
-                .servers(List.of(devServer,prodServer));
+                .servers(List.of(devServer));
     }
 }

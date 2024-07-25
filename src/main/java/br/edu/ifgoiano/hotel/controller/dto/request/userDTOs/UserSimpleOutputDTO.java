eebfdfde875aae30c01fb3c1abace78dde650204
Flávio Diniz de Sousa <dinizdesousaflavio@gmail.com>
2024-07-25 07:54:38 -0300

    25/07/24 -  v1 - DTOs Usuario

--- src/main/java/br/edu/ifgoiano/hotel/controller/dto/request/userDTOs/UserSimpleOutputDTO.java
+++ src/main/java/br/edu/ifgoiano/hotel/controller/dto/request/userDTOs/UserSimpleOutputDTO.java
@@ -3,12 +3,15 @@ package br.edu.ifgoiano.hotel.controller.dto.request.userDTOs;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@Setter
 public class UserSimpleOutputDTO {
     private Long id;
-    private String nome;
+    private String firstName;
+    private String lastName;
     private String email;
 }
